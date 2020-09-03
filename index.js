2f86445e9ef071875bb19d1708de674d915473a0
kidsonfilms-python-rules <58677791+kidsonfilms-python-rules@users.noreply.github.com>
2020-09-02 20:56:28 -0700

    Token Fix for Heroku

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ var scheduledMeme = schedule.scheduleJob('00 19 * * *', function(){
 
 
 //Lead Dev will give token
-
-client.login('')
+client.login('NzM3NDI0Mzk3MDc3NDQ2Njg4.Xx9J_A.gLYR3VPF5hgqMW5NSTTE5UFHhrg')
 
 //52472962
+
