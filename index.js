e3b88ba3ebd7c201e8751457d2c66d281ff7aa51
kidsonfilms-python-rules <58677791+kidsonfilms-python-rules@users.noreply.github.com>
2020-09-05 08:45:51 -0700

    Commiting UNFINISHED CODE

--- index.js
+++ index.js
@@ -49,16 +49,21 @@ client.once('ready', () => {
 client.on('message', async message => {
     //MODERATION
     var Filter = require('bad-words'),
-    filter = new Filter({ placeHolder: '򯾁'});
+        filter = new Filter({ placeHolder: '򯾁' });
+
+    //EASTER EGG
+    if (message.content.toString().toLowerCase().includes('suck')) {
+        message.reply('no u')
+    }
 
     // console.log(filter.clean(message.content));
     if (filter.clean(message.content).includes('򯾁')) {
         message.delete()
-        const mutedEmbed = new Discord.MessageEmbed().setColor("#FF0000").setTitle("You Have Been Muted").setDescription("You have been muted for saying ```" + message.content + "``` If you think this is a mistake, contact an @Admin")
+        const mutedEmbed = new Discord.RichEmbed().setColor("#FF0000").setTitle("You Have Been Muted").setDescription("You have been muted for saying ```" + message.content + "``` If you think this is a mistake, contact an @Admin")
         message.author.send(mutedEmbed)
         message.channel.send(message.author.toString() + "** has broken Rebel Retreat's Rules by Saying Blacklisted Words**")
     }
-    
+
     //checks if it starts with '!' or the bot sent it. if not then returns the function
     if (!message.content.includes(prefix) || message.author.bot) return;
 
@@ -73,19 +78,20 @@ client.on('message', async message => {
 
     if (command === prefix + 'help') {
         if (commandList[1] == 'admin') {      //message.sender.roles.find(r => r.name === "Admin") || message.member.roles.find(r => rname === "Mod"
-            var adminhelpembed = new Discord.MessageEmbed()
+            var adminhelpembed = new Discord.RichEmbed()
                 .setColor('#E96A00')
                 .setTitle('Commands List')
                 .addFields(
                     { name: '‎', value: '‎' },
                     { name: '**-ping**', value: 'Gives the ping.' },
+                    { name: '**-ticketclose `#channelId`**', value: 'Closes a ticket. Replace `#channelId` with the channel id of your ticket' }
 
                 )
                 .setFooter('rebel bot • Admin Help Menu')
             message.channel.send(adminhelpembed)
         } else {
 
-            var helpembed = new Discord.MessageEmbed()
+            var helpembed = new Discord.RichEmbed()
                 .setColor('#E96A00')
                 .setTitle('Commands List')
                 .setDescription('**If you want Admin Help, do `-help admin`**')
@@ -94,6 +100,8 @@ client.on('message', async message => {
                     { name: '**-help**', value: 'Shows a list of all the commands.' },
                     { name: '**-animals**', value: 'Gives a random cute animal picture! :smiley:' },
                     { name: '**-meme**', value: 'Returns a meme from Reddit' },
+                    { name: '**-openticket**', value: 'Opens a ticket' },
+                    { name: '**-closeticket**', value: 'Closes a ticket' },
 
                 )
                 .setFooter('rebel bot • Help Menu')
@@ -141,7 +149,7 @@ client.on('message', async message => {
                                 console.log(body.url + "has been sent to " + message.author);
                                 message.channel.send("Here you go " + message.author.toString() + " directly from r/" + body.subreddit.toString() + "(" + body.postLink + ")", { files: [body.url] })
                             } else {
-                                const errorEmbed = new Discord.MessageEmbed()
+                                const errorEmbed = new Discord.RichEmbed()
                                     .setColor('#FF0000')
                                     .setTitle('ERROR 376  TEST')
                                     .setAuthor('ERROR', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
@@ -158,12 +166,70 @@ client.on('message', async message => {
             }
         });
         message.delete()
+    } else if (command === prefix + 'openticket') {
+        var name = "ticket-" + message.author.username.toString().toLowerCase() + "-" + Math.floor(Math.random() * (999999 - 100000 + 1) + 100000).toString()
+        message.guild.roles.create({
+            data: {
+                name: name,
+                color: 'DEFAULT',
+            },
+            reason: 'Role to open ' + name + " ticket",
+        }).then(() => {
+            const role = message.guild.roles.cache.find(role => role.name === name);
+            message.member.roles.add(role);
+            var ticketChannel = message.guild.channels.create(name, 'text', [{
+                type: 'role',
+                id: message.guild.roles.cache.find(role => role.name === '@everyone').id,
+                deny: ['VIEW_CHANNEL']
+            }, {
+                type: 'role',
+                id: role,
+                allow: ['SEND_MESSAGES', "READ_MESSAGES"]
+
+            }]
+            ).then((c) => {
+                var ticketChannel = client.channels.cache.find(channel => channel.name === name)
+                var adminRoleId = message.guild.roles.cache.find(role => role.name === "Admin");
+                var modRoleId = message.guild.roles.cache.find(role => role.name === "Moderator");
+                var vipRoleId = message.guild.roles.cache.find(role => role.name === "VIPs");
+                ticketChannel.send(`${adminRoleId} ${modRoleId}, ` + message.author.toString() + " has opened a ticket. " + message.author.toString() + `, If you have opened this ticket to get {vipRoleId} state your real first name and if you know anyone, please state who you know.`)
+                message.reply(`**Ticket Created! Your ticket is <#${ticketChannel.id.toString()}>.` + ' Use `-closeticket` in <#' + ticketChannel.id.toString() + '> to close it!**')
+                message.delete()
+            })
+        })
+    } else if (command == prefix + 'closeticket') {
+        if (message.channel.name.includes('ticket')) {
+            message.channel.setName('closed-ticket-' + message.channel.name)
+            message.channel.delete('Closing ticket ' + message.channel.name)
+            const role = message.guild.roles.cache.find(role => role.name === message.channel.name);
+            role.delete()
+            console.log('SAME CHANNEL')
+        } else if (message.member.roles.find(r => r.name === "Admin") || message.member.roles.find(r => r.name === "Moderator")) {
+            if (commandList.length >= 2) {
+                try { var ticketChannel = client.channels.cache.find(channel => channel.id === commandList[1].replace('<', '').replace('>', '').replace('#', '')) } catch (err) {
+                    message.channel.send(`**Ticket Has failed to close. ERORR: ${err.toString()}. **`)
+                    message.delete()
+                }
+                // ticketChannel.send('THIS FINALLY WORKS!! :fireworks: :smiley: :fireworks:')
+                try { ticketChannel.setName('closed-ticket-' + ticketChannel.name) } catch (err) {
+                    message.channel.send(`**Ticket Has failed to close. ERORR: ${err.toString()}. **`)
+                    message.delete()
+                }
+                ticketChannel.delete()
+                const role = message.guild.roles.cache.find(role => role.name === ticketChannel.name);
+                role.delete()
+                // console.log(commandList[1])
+            }
+        } else {
+            message.reply('Ticket has failed to close. Please make sure you have proper permissions or have no typos. Here is your failed command: ```sh\n' + message.content.toString() + '\n```')
+            message.delete()
+        }
     }
 
 
 
 
-    
+
 
 
     //else if (command === prefix + 'cube-record') {
@@ -212,52 +278,52 @@ client.on('guildMemberAdd', m => {
 })
 
 //MEME SCHEDULE
-var scheduledMeme = schedule.scheduleJob('00 19 * * *', function(){
-     // https.get("https://meme-api.herokuapp.com/gimme", (s) => {
-        //     console.log(s)
-        // })
-        const request = require('request');
-        const memeChannel = client.channels.cache.find(channel => channel.name === "dank-memes")
+var scheduledMeme = schedule.scheduleJob('00 19 * * *', function () {
+    // https.get("https://meme-api.herokuapp.com/gimme", (s) => {
+    //     console.log(s)
+    // })
+    const request = require('request');
+    const memeChannel = client.channels.cache.find(channel => channel.name === "dank-memes")
 
-        request('https://meme-api.herokuapp.com/gimme', { json: true }, (err, res, body) => {
-            if (err) { return console.log(err); }
-            textract.fromUrl(body.url, function (error, text) {
-                console.log(body.url + '   ' + text)
-            })
+    request('https://meme-api.herokuapp.com/gimme', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        textract.fromUrl(body.url, function (error, text) {
+            console.log(body.url + '   ' + text)
+        })
 
-            if (body.nsfw == true) {
-                console.log(body.url + " Is today's daily meme");
-                memeChannel.send("Here's today's daily meme directly from r/" + body.subreddit.toString() + " (" + body.postLink + ") Enjoy!", { files: [body.url] })
-            } else {
-                request('https://meme-api.herokuapp.com/gimme', { json: true }, (err, res, body) => {
-                    if (err) { return console.log(err); }
-                    if (body.nsfw == false) {
-                        console.log(body.url + " Is today's daily meme");
-                        memeChannel.send("Here's today's daily meme directly from r/" + body.subreddit.toString() + " (" + body.postLink + ") Enjoy!", { files: [body.url] })
-                    } else {
-                        request('https://meme-api.herokuapp.com/gimme', { json: true }, (err, res, body) => {
-                            if (err) { return console.log(err); }
-                            if (body.nsfw == false) {
-                                console.log(body.url + " Is today's daily meme");
-                                memeChannel.send("Here's today's daily meme directly from r/" + body.subreddit.toString() + " (" + body.postLink + ") Enjoy!", { files: [body.url] })
-                            } else {
-                                const errorEmbed = new Discord.MessageEmbed()
-                                    .setColor('#FF0000')
-                                    .setTitle('ERROR 376  TEST')
-                                    .setAuthor('ERROR', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
-                                    .setDescription('An Error Has Occurred. Please Try Agian')
-                                    .setThumbnail('https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
-                                    .setTimestamp()
-                                    .setFooter('Error TEST', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png');
+        if (body.nsfw == true) {
+            console.log(body.url + " Is today's daily meme");
+            memeChannel.send("Here's today's daily meme directly from r/" + body.subreddit.toString() + " (" + body.postLink + ") Enjoy!", { files: [body.url] })
+        } else {
+            request('https://meme-api.herokuapp.com/gimme', { json: true }, (err, res, body) => {
+                if (err) { return console.log(err); }
+                if (body.nsfw == false) {
+                    console.log(body.url + " Is today's daily meme");
+                    memeChannel.send("Here's today's daily meme directly from r/" + body.subreddit.toString() + " (" + body.postLink + ") Enjoy!", { files: [body.url] })
+                } else {
+                    request('https://meme-api.herokuapp.com/gimme', { json: true }, (err, res, body) => {
+                        if (err) { return console.log(err); }
+                        if (body.nsfw == false) {
+                            console.log(body.url + " Is today's daily meme");
+                            memeChannel.send("Here's today's daily meme directly from r/" + body.subreddit.toString() + " (" + body.postLink + ") Enjoy!", { files: [body.url] })
+                        } else {
+                            const errorEmbed = new Discord.MessageEmbed()
+                                .setColor('#FF0000')
+                                .setTitle('ERROR 376  TEST')
+                                .setAuthor('ERROR', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
+                                .setDescription('An Error Has Occurred. Please Try Agian')
+                                .setThumbnail('https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png')
+                                .setTimestamp()
+                                .setFooter('Error TEST', 'https://cdn0.iconfinder.com/data/icons/shift-free/32/Error-512.png');
 
-                                memeChannel.send(errorEmbed);
-                            }
-                        });
-                    }
-                });
-            }
-        });
-  });
+                            memeChannel.send(errorEmbed);
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
 
 
 //Lead Dev will give token
